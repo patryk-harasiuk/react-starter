8d9d78e92087f081caa78f06856b13fae4e029ba
Patryk Harasiuk <patryk.harasiuk@op.pl>
2022-04-25 00:37:59 +0200

    adjust tsconfig.json, add tsconfig.paths.json file

---
+++ src/components/app.tsx
@@ -0,0 +1,4 @@
+import { Store } from "redux";
+import { Provider } from "react-redux";
+import { ThemeProvider, CssBaseline, Theme } from "@material-ui/core";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
