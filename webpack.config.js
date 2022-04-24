42907d8c73579c18dfb2dff373db1822538bd207
Patryk Harasiuk <patryk.harasiuk@op.pl>
2022-04-25 00:09:01 +0200

    add getEnvironment function and BundleAnalyzerPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -2,40 +2,55 @@ const path = require("path");
 
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
+const BundleAnalyzerPlugin =
+  require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
+
+const ENV = getEnvironment();
+
+function getEnvironment() {
+  const envOptions = ["development", "production", "none"];
+  const env = process.env.NODE_ENV;
+
+  return envOptions.includes(env) ? env : "production";
+}
 
 module.exports = {
-    entry: "./src/index.ts",
-    output: {
-        path: path.resolve(__dirname, "dist"),
-        filename: "output.js",
-    },
+  entry: path.resolve(__dirname, "src/index.ts"),
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "output.js",
+  },
 
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                use: "ts-loader",
-                exclude: /node_modules/,
-            },
-        ],
-    },
+  mode: ENV,
 
-    resolve: {
-        extensions: [".tsx", ".ts", ".js"],
-    },
-
-    devServer: {
-        port: 4000,
-    },
-
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: "./public/index.html",
-            inject: "head",
-            scriptLoading: "defer",
-        }),
-        // new CopyWebpackPlugin({
-        //   patterns: [{ from: "./src/global.css", to: "./style.css" }],
-        // }),
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
     ],
+  },
+
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+
+  devServer: {
+    port: 4000,
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+      inject: "head",
+      scriptLoading: "defer",
+    }),
+    new BundleAnalyzerPlugin(),
+
+    // new CopyWebpackPlugin({
+    //   patterns: [{ from: "./src/global.css", to: "./style.css" }],
+    // }),
+  ],
 };
